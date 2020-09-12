9ae4c3914737842e58c721ff355ea5e21b8fe9c7
ashawkey <ashawkey1999@gmail.com>
2020-09-12 10:04:40 +0000

    luna

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from flask_cors import CORS
 from flask import Flask, request, jsonify
 
 import route
-import mysql
 
 
 app = Flask(__name__, instance_relative_config=True)
@@ -16,9 +15,8 @@ CORS(app)
 app.config.from_object('config')
 
 app.register_blueprint(route.bp)
-mysql.init_app(app)
 
 
 if __name__ == "__main__":
     #app.run("0.0.0.0", 5000, ssl_context=('cert.pem', 'key.pem'))
-    app.run("127.0.0.1", 5000)
+    app.run("127.0.0.1", 8002)
--- route.py
+++ route.py
@@ -1,8 +1,10 @@
 import time
 from flask import Blueprint, request, jsonify
-from mysql import *
 
-bp = Blueprint('route', __name__, url_prefix='/api/nonsense/')
+from googleapiclient.discovery import build
+from const import *
+
+bp = Blueprint('route', __name__, url_prefix='/api/umbra/')
 
 #@bp.route('/')
 #def hello():
@@ -11,103 +13,23 @@ bp = Blueprint('route', __name__, url_prefix='/api/nonsense/')
 # =========== Nonsense ===========
 # currently, we leave exception handling with frontend. 
 
-@bp.route('/meta')
-def get_nonsense_meta():
-    token = request.args.get('token')
-    db = mysql.get_db()
-    cur = db.cursor()
-    cur.execute(stmt_meta, (token,))
-    meta = cur.fetchall()
-    cur.close()
-    db.close()
-    return jsonify(meta)
-
-@bp.route('/post', methods=('POST', ))
-def post_nonsense():
-    ctime = time.time()
-    mtime = ctime
-    body = request.form['body']
-    token = request.form['token']
-    state = request.form['state']
-
-    db = mysql.get_db()
-    cur = db.cursor()
-    cur.execute(stmt_post, (ctime, mtime, body, token, state))
-    db.commit()
-
-    # get nid
-    cur.execute(stmt_size)
-    nid = cur.fetchone()[0]
-    cur.close()
-    db.close()
-
-    return jsonify({
-        'success': True,
-        'nid': nid,
-        })
-
-@bp.route('/get')
-def get_nonsense_content():
-    nid = request.args.get('nid')
-    db = mysql.get_db()
-    cur = db.cursor()
-    cur.execute(stmt_get, (nid,))
-    content = cur.fetchone()
-    cur.close()
-    db.close()
-    if content is None:
-        return jsonify({
-            'success': False,
-        })
-    else:
-        return jsonify({
-            'success': True,
-            'content': content,
-        })
-
-@bp.route('/update', methods=('POST', ))
-def update_nonsense_content():
-    mtime = time.time()
-    body = request.form['body']
-    state = request.form['state']
-    nid = request.form['nid']
-
-    db = mysql.get_db()
-    cur = db.cursor()
-    cur.execute(stmt_update, (mtime, body, state, nid))
-    db.commit()
-    cur.close()
-    db.close()
-
-    return jsonify({
-        'success': True,
-        })
 
 @bp.route('/search')
 def search_nonsense_content():
-    token = request.args.get('token')
     keyword = request.args.get('keyword')
+    page = int(request.args.get('page'))
+    print(keyword, page)
 
-    db = mysql.get_db()
-    cur = db.cursor()
-    cur.execute(stmt_search, (keyword, token))
-    res = cur.fetchall()
-    cur.close()
-    db.close()
+    start = (page - 1) * 10 + 1
 
-    return jsonify(res)
+    def google_search(search_term, api_key, cse_id, **kwargs):
+        service = build("customsearch", "v1", developerKey=api_key)
+        res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
+        return res
 
-@bp.route('/delete', methods=('POST', ))
-def delete_nonsense_content():
-    nid = request.form['nid']
+    res = google_search(keyword, api_key, cse_id, num=10, start=start)
 
-    db = mysql.get_db()
-    cur = db.cursor()
-    cur.execute(stmt_delete, (nid,))
-    db.commit()
-    cur.close()
-    db.close()
-    
-    return jsonify({
-        'success': True,
-        })
+    if 'items' in res:
+        return jsonify(res['items'])
+    else:
+        return jsonify({})
